f7f9d516119b2153d8315d072e636d75c3a48cdb
Patricia <patriciafaustinobr@gmail.com>
2020-09-27 19:09:28 -0300

    Programa com concantenação em placheholders

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2 {
+    class Program {
+        static void Main(string[] args) { 
+            int idade = 32;
+            double saldo = 10.35784;
+            String nome = "Maria";
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+          
+        }
+    }
+}
+
